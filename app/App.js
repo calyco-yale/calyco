11f6fbfdea3fdf456bfa0a709612e9cdc9a470c1
Yunji Jong <yunji.jong@yale.edu>
2021-03-13 19:40:45 -0800

    created app folder

---
+++ app/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, ImageBackground } from 'react-native';
+
+import Amplify from 'aws-amplify'
+import config from '../aws-exports'
+import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
+Amplify.configure(config)
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground 
+        source= {require("./assets/calyco-cat-2.jpg")}
+        style={{
+          height:'100%',
+          width:'100%',
+          position: 'relative'
+        }}
+      >
+        <Button
+          title="Create Account"
+          color="#841584"
+          position = "absolute"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Text style={{textAlignVertical: "center", textAlign: "center",}}>Calyco</Text>
+      </ImageBackground>
+      
+      <StatusBar style="auto"/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
