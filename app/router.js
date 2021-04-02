c78083e8c32df1b2e553b833eb00919ab08a5732
Yunji Jong <yunji.jong@yale.edu>
2021-04-02 17:03:18 +0900

    navbar works

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Drawer, Router, Scene, Actions, Stack, Tabs } from 'react-native-router-flux';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 // import HomeScreen from './screens/HomeScreen';
 import Colors from '../src/constants/colors';
@@ -14,30 +14,30 @@ import ProfilePage from './screens/ProfilePage';
 import NavBar from './components/NavBar';
 import NewsFeedScreen from './screens/NewsFeedScreen';
 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 const AppRouter = () => (
   <Router>
     <Scene key="root" title="">
       <Scene
         key="loginScreen"
-        component={LoginScreen}
         initial
+        component={LoginScreen}
         gesturesEnabled={false}
         hideNavBar
       />
-      <Scene
+
+      {/* <Scene
         key="signupScreen"
         component={SignupScreen}
         hideNavBar
       />
+
       <Scene
         key="verificationScreen"
         component={VerificationScreen}
         hideNavBar
-      />
-      <Scene
-        key="searchScreen"
-        component={SearchScreen}
       />
 
       <Scene
@@ -53,87 +53,14 @@ const AppRouter = () => (
       <Scene
         key="friendRequestScreen"
         component = {FriendRequestScreen}
-      />
+      /> */}
 
-      <Stack>
-        <Scene key='tabBar' tabs={true} tabBarStyle={styles.tabBar} default='Main'>
-          <Scene key='newsFeed' component={NewsFeedScreen} hideNavBar={true} title='News Feed'/>
-          <Scene key='searchScreen' component={SearchScreen} hideNavBar={true} title='Search Screen'/>
-          <Scene key ='profilePage' component={ProfilePage} hideNavBar={true} title='Profile Page'/>
-        </Scene>
-      </Stack>
-
-      {/* <Drawer
-        key="drawer"
-        hideNavBar
-        contentComponent={SideDrawer}
-        drawerIcon={<DrawerImage />}
-        panHandlers={null}
-        drawerWidth={300}
-      >
-        <Scene>
-          <Scene
-            key="homeScreen"
-            component={HomeScreen}
-            title="Restaurant App"
-            titleStyle={{
-              fontFamily: 'Roboto Slab',
-              color: Colors.primaryColor,
-            }}
-          />
-
-          <Scene
-            key="cuisineRestaurants"
-            component={CuisineRestaurantsScreen}
-            titleStyle={{
-              fontFamily: 'Roboto Slab',
-              color: Colors.primaryColor,
-            }}
-          />
-
-          <Scene
-            key="restaurantScreen"
-            component={RestaurantInfoScreen}
-          />
-
-          <Scene
-            key="cartScreen"
-            component={CartScreen}
-            navigationBarStyle={{
-              backgroundColor: '#fff',
-              elevation: 2,
-              borderBottomWidth: 1,
-              borderBottomColor: '#eee',
-            }}
-            titleStyle={{
-              fontFamily: 'Roboto Slab',
-              color: Colors.primaryColor,
-            }}
-            title="Cart"
-          />
-
-          <Scene
-            drawer={false}
-            key="paymentHome"
-            component={PaymentHome}
-          />
-
-          <Scene
-            key="paymentSuccess"
-            component={PaymentComplete}
-          />
-
-          <Scene
-            key="paymentFailed"
-            component={PaymentFailed}
-          />
-          <Scene
-            key="showAllOrders"
-            component={OrdersList}
-            title="My Orders"
-          />
-        </Scene>
-      </Drawer> */}
+      <Scene key='tabBar' hideNavBar tabs={true} tabBarStyle={styles.tabBar} default='Main'>
+        <Scene key='newsFeed' component={NewsFeedScreen} hideNavBar={true} icon={NewsFeedTab} title='News Feed'/>
+        <Scene key='searchScreen' component={SearchScreen} hideNavBar={true} icon={SearchTab} title='Search Screen'/>
+        <Scene key ='profilePage' component={ProfilePage} hideNavBar={true} icon={ProfileTab} title='Profile Page'/>
+      </Scene>
+    
     </Scene>
   </Router>
 );
@@ -147,5 +74,47 @@ const styles = StyleSheet.create({
     justifyContent:'space-between'
   }
 });
+
+const NewsFeedTab = (props) => {
+  let textColor = props.focused ? '#333333' : '#999999'
+  // const settingImageFocused = require("Give your image path")
+  // const settingImageUnfocused = require("Give your image path")
+  // let settingImage = props.focused ? settingImageFocused : settingImageUnfocused
+  let borderColor = props.focused ? '#333333' : '#FFFFFF'
+  return (
+  <View style={{flex: 1, flexDirection:'column', alignItems:'center', justifyContent:'center', borderTopColor: borderColor, borderTopWidth:4, padding:20}}>
+  <MaterialCommunityIcons name="home" color= '#000000' size={26} />
+  <Text style={{color: textColor}}>Settings</Text>
+  </View>
+  );
+}
+
+const SearchTab = (props) => {
+  let textColor = props.focused ? '#333333' : '#999999'
+  // const settingImageFocused = <MaterialCommunityIcons name="home" color={color} size={26} />
+  // const settingImageUnfocused = require("Give your image path")
+  // let settingImage = props.focused ? settingImageFocused : settingImageUnfocused
+  let borderColor = props.focused ? '#333333' : '#FFFFFF'
+  return (
+  <View style={{flex: 1, flexDirection:'column', alignItems:'center', justifyContent:'center', borderTopColor: borderColor, borderTopWidth:4, padding:20}}>
+  <MaterialCommunityIcons name="bell" color= '#000000' size={26} />
+  <Text style={{color: textColor}}>Settings</Text>
+  </View>
+  );
+}
+
+const ProfileTab = (props) => {
+  let textColor = props.focused ? '#333333' : '#999999'
+  // const settingImageFocused = require("Give your image path")
+  // const settingImageUnfocused = require("Give your image path")
+  // let settingImage = props.focused ? settingImageFocused : settingImageUnfocused
+  let borderColor = props.focused ? '#333333' : '#FFFFFF'
+  return (
+  <View style={{flex: 1, flexDirection:'column', alignItems:'center', justifyContent:'center', borderTopColor: borderColor, borderTopWidth:4, padding:20}}>
+  <MaterialCommunityIcons name="account" color= '#000000' size={26} />
+  <Text style={{color: textColor}}>Settings</Text>
+  </View>
+  );
+}
 
 export default AppRouter;
