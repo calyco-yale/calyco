108e3304e332719f7d1820c03ea345ca76a19263
stanleywbwong <swong394@gmail.com>
2021-04-14 18:35:23 -0400

    add push notification on sending friend request

--- app/helpers.js
+++ app/helpers.js
@@ -8,6 +8,8 @@ import { getUser, usersByEmail } from '../src/graphql/queries';
 import { getUsersByEmail } from '../src/graphql/custom_queries';
 import { createSimpleFriendship, deleteFriendshipById } from '../src/graphql/custom_mutations';
 
+import * as Notifications from 'expo-notifications';
+
 // Get currently logged in user
 export const getloggedInUser = async () => {
   const { attributes } = await Auth.currentAuthenticatedUser();
@@ -181,3 +183,29 @@ export const userItemSeparator = () => {
     />
   );
 };
+
+// Sends a friend notification to the device associated with expoPushToken
+export const sendFriendNotification = async (expoPushToken) => {
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Calyco',
+    body: 'You have a pending friend request!',
+  };
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
