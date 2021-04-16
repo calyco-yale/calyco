4a6f880c97eeedad5244d59e6d905317e5dd7885
stanleywbwong <swong394@gmail.com>
2021-04-16 16:56:29 -0400

    customize user profile calendar

--- app/components/CalendarEvent.js
+++ app/components/CalendarEvent.js
@@ -150,6 +150,30 @@ class CalendarEvent extends Component {
               // 2. if pressed date is in list of event dates => alert
             }}
             style={{width: 350}}
+            theme={{
+              backgroundColor: '#ffffff',
+              calendarBackground: '#ffffff',
+              textSectionTitleColor: '#b6c1cd',
+              textSectionTitleDisabledColor: '#d9e1e8',
+              selectedDayBackgroundColor: '#00adf5',
+              selectedDayTextColor: '#ffffff',
+              todayTextColor: '#00adf5',
+              dayTextColor: '#2d4150',
+              textDisabledColor: '#d9e1e8',
+              dotColor: '#00adf5',
+              selectedDotColor: '#ffffff',
+              arrowColor: 'orange',
+              disabledArrowColor: '#d9e1e8',
+              monthTextColor: 'orange',
+              indicatorColor: 'orange',
+              textDayFontFamily: 'Futura',
+              textDayFontWeight: '300',
+              textMonthFontWeight: 'bold',
+              textDayHeaderFontWeight: '300',
+              textDayFontSize: 16,
+              textMonthFontSize: 16,
+              textDayHeaderFontSize: 16
+            }}
           />
         </View>
       );
