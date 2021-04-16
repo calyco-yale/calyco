578602777058bd4332eaae48e43b306b3819c35a
Brian Rhee <byungyeonrhee@gmail.com>
2021-04-16 00:09:03 -0400

    basic time functions

--- app/helpers.js
+++ app/helpers.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AppBase from "./base_components/AppBase";
 import UserComponent from "./components/User";
-
+import global from './global';
 import { Auth } from "aws-amplify";
 import { API, graphqlOperation } from "aws-amplify";
 import { getUser, usersByEmail } from "../src/graphql/queries";
@@ -9,6 +9,24 @@ import {
   createSimpleFriendship,
   deleteFriendshipById
 } from "../src/graphql/custom_mutations";
+
+
+export const retrieveOffset = () => {
+  const date = new Date();
+  const offset = date.getTimezoneOffset();
+  // console.log(date);
+  // console.log(offset);
+  global.offset = offset;
+  return offset;
+}
+
+//sign for whether the user is recieving or sending time requests
+export const getCorrectTime = (date, sign) => {
+  const offset = global.offset;
+  var currDate = new Date(date);
+  currDate.setMinutes(currDate.getMinutes() + (sign)*offset);
+  return new Date(currDate);
+}
 
 // Get currently logged in user
 export const getloggedInUser = async () => {
