b0a6e67502b87d0702b8d959399f9bad0563e071
Jinny Choi <jchoi@Jinnys-MacBook-Air.local>
2021-04-16 09:01:26 +0900

    Friends List FE Done

--- app/components/User.js
+++ app/components/User.js
@@ -1,19 +1,54 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
-import TextButton from '../base_components/TextButton';
-import { Actions } from 'react-native-router-flux';
+import React, { Component } from "react";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+import TextButton from "../base_components/TextButton";
+import { Actions } from "react-native-router-flux";
 
 class UserComponent extends Component {
   render() {
-    const {userItem} = this.props;
+    const { userItem } = this.props;
     return (
-      <TextButton size={26} primary
-          onPress={() => Actions.userProfileScreen({userId: userItem.id})}
-          // title={[userItem.first_name, userItem.last_name].join(' ')}
-          title={userItem.username}
-        />
+      <TouchableOpacity
+        style={styles.listItem}
+        onPress={() => Actions.userProfileScreen({ userId: userItem.id })}
+      >
+        <View style={styles.listView}>
+          <Image
+            style={styles.profileImage}
+            source={{ uri: userItem.image_url }}
+          />
+          <Text style={styles.userText}>
+            {userItem.first_name} {userItem.last_name}
+          </Text>
+        </View>
+      </TouchableOpacity>
     );
   }
-};
+}
 
+const styles = StyleSheet.create({
+  listItem: {
+    width: "100%",
+    padding: 15,
+    // backgroundColor: "#f8f8f8",
+    borderBottomWidth: 1
+  },
+  listView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  userText: {
+    width: "100%",
+    fontSize: 25,
+    top: 10
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    position: "relative",
+    padding: 15,
+    marginRight: 15
+  }
+});
 export default UserComponent;
