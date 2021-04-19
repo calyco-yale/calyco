c33d79e960c666cae43c06c9521f827592cf3011
Jinny Choi <jchoi@Jinnys-MacBook-Air.local>
2021-04-19 15:10:41 +0900

    Participants FrontEnd

--- app/components/CreateEvent.js
+++ app/components/CreateEvent.js
@@ -1,22 +1,31 @@
-import React, { Component, useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import debounce from 'lodash/debounce';
-import { Actions } from 'react-native-router-flux';
-import { Button, Text, Image, View, Platform, StyleSheet, TouchableOpacity } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { Component, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import debounce from "lodash/debounce";
+import { Actions } from "react-native-router-flux";
+import {
+  Button,
+  Text,
+  Image,
+  View,
+  Platform,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+import AppBase from "../base_components/AppBase";
+import PrimaryText from "../base_components/PrimaryText";
+import BR from "../base_components/BR";
+import TextInput from "../base_components/TextInput";
+import RoundButton from "../base_components/RoundButton";
+import TextButton from "../base_components/TextButton";
+import EventImage from "../components/EventImage";
+import { ScrollView } from "react-native-gesture-handler";
 
-import AppBase from '../base_components/AppBase';
-import PrimaryText from '../base_components/PrimaryText';
-import BR from '../base_components/BR';
-import TextInput from '../base_components/TextInput';
-import RoundButton from '../base_components/RoundButton';
-import TextButton from '../base_components/TextButton';
-import EventImage from '../components/EventImage';
-import { ScrollView } from 'react-native-gesture-handler';
+import DatePicker from "react-native-datepicker";
 
-import DatePicker from 'react-native-datepicker'
+import Colors from "../../src/constants/colors";
 
 const createDateTime = () => {
   var date = new Date().getDate(); //To get the Current Date
@@ -25,247 +34,288 @@ const createDateTime = () => {
   var hours = new Date().getHours(); //To get the Current Hours
   var min = new Date().getMinutes();
 
-  var dateTimeString = year + "-" + month + "-" + date + " " + hours + ":" + min;
-  return dateTimeString
+  var dateTimeString =
+    year + "-" + month + "-" + date + " " + hours + ":" + min;
+  return dateTimeString;
 };
 
-
 class CreateEventComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            image: null,
-            datetime: createDateTime(),
-            datetime1: createDateTime()
-        };
-    }
-
-    pickImage = async () => {
-      const { onEventImageChange } = this.props;
-        if (Platform.OS !== 'web') {
-            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            if (status !== 'granted') {
-                alert('Sorry, we need camera roll permissions to make this work!');
-            } else {
-                let result = await ImagePicker.launchImageLibraryAsync({
-                    mediaTypes: ImagePicker.MediaTypeOptions.All,
-                    allowsEditing: true,
-                    aspect: [4, 3],
-                    quality: 1,
-                });
-            
-                if (!result.cancelled) {
-                    this.setState({image: result.uri});
-                    onEventImageChange(result.uri);
-                }
-            }
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      datetime: createDateTime(),
+      datetime1: createDateTime()
     };
+  }
+
+  pickImage = async () => {
+    const { onEventImageChange } = this.props;
+    if (Platform.OS !== "web") {
+      const {
+        status
+      } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      } else {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1
+        });
+
+        if (!result.cancelled) {
+          this.setState({ image: result.uri });
+          onEventImageChange(result.uri);
+        }
+      }
+    }
+  };
 
   render() {
     const {
-      loading, registerMessage, registerError,
-      onEventCreationSubmit, onPublicChange,
+      loading,
+      registerMessage,
+      registerError,
+      onEventCreationSubmit,
+      onPublicChange,
       onEventNameChange,
-      onStartTimeChange, onEndTimeChange,
-      onEventImageChange, disableCreateEvent,
-      onDescriptionChange, onParticipantsChange,
+      onStartTimeChange,
+      onEndTimeChange,
+      onEventImageChange,
+      disableCreateEvent,
+      onDescriptionChange,
+      onParticipantsChange,
       user,
       participants
     } = this.props;
 
     if (registerMessage && registerMessage.success) {
-      Actions.replace('loginScreen', {
+      Actions.replace("loginScreen", {
         loginError: {
-          message: 'Sign Up successful',
-        },
+          message: "Sign Up successful"
+        }
       });
     }
 
     return (
-        <ScrollView>
-      <AppBase
-        style={{
-          justifyContent: 'flex-start',
-        }}
-      >
-        <BR size={25} />
-        {registerError && <PrimaryText>{registerError.message}</PrimaryText>}
-        {registerMessage && <PrimaryText>{JSON.stringify(registerMessage)}</PrimaryText>}
-        <BR size={50} />
+      <ScrollView>
+        <AppBase
+          style={{
+            justifyContent: "flex-start"
+          }}
+        >
+          <BR size={25} />
+          {registerError &&
+            <PrimaryText>
+              {registerError.message}
+            </PrimaryText>}
+          {registerMessage &&
+            <PrimaryText>
+              {JSON.stringify(registerMessage)}
+            </PrimaryText>}
+          <BR size={50} />
 
-        <Text style = {styles.Title}>Create Event </Text>
+          <Text style={styles.Title}>Create Event </Text>
 
-        <TextInput
-          autoCorrect={false}
-          onChangeText={debounce(onEventNameChange, 500)}
-          style={{
-            width: '80%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-          }}
-          underlineColorAndroid="#B9B9B9"
-          placeholder="*Event Name (eg: birthday)"
-        />
-        <TextInput
-          autoCorrect={false}
-          onChangeText={debounce(onPublicChange, 500)}
-          style={{
-            width: '80%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-          }}
-          underlineColorAndroid="#B9B9B9"
-          placeholder="*Public (eg: true/false)"
-        />
-        <TextInput
-          autoCorrect={false}
-          onChangeText={debounce(onDescriptionChange, 500)}
-          style={{
-            width: '80%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-          }}
-          underlineColorAndroid="#B9B9B9"
-          placeholder="*Description (eg: alex's bday)"
-        />
-        <BR />
-        <BR>
-        <Text>Start Date Time</Text>
-        </BR>
-        <DatePicker
-          style={{width: 300}}
-          date={this.state.datetime}
-          mode="datetime"
-          format="YYYY-MM-DD HH:mm"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 0
-            },
-            dateInput: {
-              marginLeft: 36
-            }
-          }}
-          onDateChange={(datetime) => {this.setState({datetime: datetime});
-            onStartTimeChange(datetime);}}
-        />
-        <BR></BR>
-        <BR>
-        <Text>End Date Time</Text>
-        </BR>
-        <DatePicker
-          style={{width: 300}}
-          date={this.state.datetime1}
-          mode="datetime"
-          format="YYYY-MM-DD HH:mm"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 0
-            },
-            dateInput: {
-              marginLeft: 36
-            }
-          }}
-          minuteInterval={10}
-          onDateChange={(datetime) => {this.setState({datetime1: datetime}); onEndTimeChange(datetime);}}
-        />
-        <BR></BR>
-        <Text>Upload Event Image</Text>
-        <View style={{ 
-            flex: 1, 
-            alignItems: 'center', 
-            justifyContent: 'center' 
-            }}>
-            {this.state.image ? 
-                <Image 
-                source={{ uri: this.state.image }} 
-                style={styles.image} 
-                onPress={this.pickImage}/> : 
-                <TouchableOpacity
-                style={styles.selected}
-                onPress={this.pickImage}
-                // {...onEventImageChange(this.state.image)}
+          <TextInput
+            autoCorrect={false}
+            onChangeText={debounce(onEventNameChange, 500)}
+            style={{
+              width: "80%",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+            underlineColorAndroid="#B9B9B9"
+            placeholder="*Event Name (eg: birthday)"
+          />
+          <TextInput
+            autoCorrect={false}
+            onChangeText={debounce(onPublicChange, 500)}
+            style={{
+              width: "80%",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+            underlineColorAndroid="#B9B9B9"
+            placeholder="*Public (eg: true/false)"
+          />
+          <TextInput
+            autoCorrect={false}
+            onChangeText={debounce(onDescriptionChange, 500)}
+            style={{
+              width: "80%",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+            underlineColorAndroid="#B9B9B9"
+            placeholder="*Description (eg: alex's bday)"
+          />
+          <BR />
+          <BR>
+            <Text>Start Date Time</Text>
+          </BR>
+          <DatePicker
+            style={{ width: 300 }}
+            date={this.state.datetime}
+            mode="datetime"
+            format="YYYY-MM-DD HH:mm"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: "absolute",
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+            }}
+            onDateChange={datetime => {
+              this.setState({ datetime: datetime });
+              onStartTimeChange(datetime);
+            }}
+          />
+          <BR />
+          <BR>
+            <Text>End Date Time</Text>
+          </BR>
+          <DatePicker
+            style={{ width: 300 }}
+            date={this.state.datetime1}
+            mode="datetime"
+            format="YYYY-MM-DD HH:mm"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: "absolute",
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+            }}
+            minuteInterval={10}
+            onDateChange={datetime => {
+              this.setState({ datetime1: datetime });
+              onEndTimeChange(datetime);
+            }}
+          />
+          <BR />
+          <Text>Upload Event Image</Text>
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            {this.state.image
+              ? <Image
+                  source={{ uri: this.state.image }}
+                  style={styles.image}
+                  onPress={this.pickImage}
+                />
+              : <TouchableOpacity
+                  style={styles.selected}
+                  onPress={this.pickImage}
+                  // {...onEventImageChange(this.state.image)}
                 >
-                <MaterialCommunityIcons name="plus-box" size={80} />
-                </TouchableOpacity>
-            }
-        </View>
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <BR />
-        <Text>{participants.map(p => <Text>{p.username}</Text>)}</Text>
-        <RoundButton
-          title="Add Participants"
-          onPress={() => Actions.addParticipantsScreen({ loggedInUser: user, participants: participants })}
-        />
-        <RoundButton
-          title="Create Event"
-          disabled={disableCreateEvent}
-          loading={loading}
-          onPress={onEventCreationSubmit}
-          buttonColor="orange"
-        />
-      </AppBase>
+                  <MaterialCommunityIcons name="plus-box" size={80} />
+                </TouchableOpacity>}
+          </View>
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <BR />
+          <Text style={styles.pText}>
+            Participants:
+            {participants.map(p =>
+              <View style={styles.pView}>
+                <Text style={styles.pText}>
+                  {p.username}
+                </Text>
+              </View>
+            )}
+          </Text>
+          <RoundButton
+            title="Add Participants"
+            onPress={() =>
+              Actions.addParticipantsScreen({
+                loggedInUser: user,
+                participants: participants
+              })}
+          />
+          <RoundButton
+            title="Create Event"
+            disabled={disableCreateEvent}
+            loading={loading}
+            onPress={onEventCreationSubmit}
+            buttonColor="orange"
+          />
+        </AppBase>
       </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    image: {
-        position: 'absolute',
-        top: 10,
-        left: -150,
-        borderWidth:1,
-        borderColor:'rgba(0,0,0,0.2)',
-        alignItems:'center',
-        justifyContent:'center',
-        width: 300,
-        height: 170,
-        backgroundColor:'#fff',
-    },
-    selected: {
-      position: 'absolute',
-      top: 10,
-      left: -150,
-      borderWidth:1,
-      borderColor:'rgba(0,0,0,0.2)',
-      alignItems:'center',
-      justifyContent:'center',
-      width: 300,
-      height: 170,
-      backgroundColor:'#fff',
-    },
-    Title: {
-      marginBottom: 10,
-      fontFamily: "Arial",
-      fontSize: 40,
-      color: "orange"
-    }
-  
+  pView: {
+    paddingRight: 5,
+    paddingLeft: 5
+  },
+  pText: {
+    fontSize: 20,
+    color: Colors.calycoColor
+  },
+  image: {
+    position: "absolute",
+    top: 10,
+    left: -150,
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 300,
+    height: 170,
+    backgroundColor: "#fff"
+  },
+  selected: {
+    position: "absolute",
+    top: 10,
+    left: -150,
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 300,
+    height: 170,
+    backgroundColor: "#fff"
+  },
+  Title: {
+    marginBottom: 10,
+    fontFamily: "Arial",
+    fontSize: 40,
+    color: "orange"
+  }
 });
 
 CreateEventComponent.defaultProps = {
   registerMessage: null,
-  registerError: null,
+  registerError: null
 };
 
 CreateEventComponent.propTypes = {
@@ -273,7 +323,7 @@ CreateEventComponent.propTypes = {
   loading: PropTypes.bool.isRequired,
   registerMessage: PropTypes.object,
   registerError: PropTypes.object,
-  onEventCreationSubmit:PropTypes.func.isRequired,
+  onEventCreationSubmit: PropTypes.func.isRequired,
   onPublicChange: PropTypes.func.isRequired,
   onEventNameChange: PropTypes.func.isRequired,
   onStartTimeChange: PropTypes.func.isRequired,
@@ -282,6 +332,5 @@ CreateEventComponent.propTypes = {
   onDescriptionChange: PropTypes.func.isRequired,
   onParticipantsChange: PropTypes.func.isRequired
 };
-
 
 export default CreateEventComponent;
--- app/screens/AddParticipantsScreen.js
+++ app/screens/AddParticipantsScreen.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Auth } from 'aws-amplify';
-import CreateEventComponent from '../components/CreateEvent';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Auth } from "aws-amplify";
+import CreateEventComponent from "../components/CreateEvent";
 
 // query imports
-import { API, graphqlOperation } from 'aws-amplify';
-import { listUsersShortened } from '../../src/graphql/custom_queries';
+import { API, graphqlOperation } from "aws-amplify";
+import { listUsersShortened } from "../../src/graphql/custom_queries";
 
-import { Actions } from 'react-native-router-flux';
-import { userItemSeparator} from '../helpers';
+import { Actions } from "react-native-router-flux";
+import { userItemSeparator } from "../helpers";
 
-import { getUser } from '../../src/graphql/queries';
-import { Alert, View, Text, FlatList, StyleSheet } from 'react-native';
-import { SearchBar } from 'react-native-elements';
-import TextButton from '../base_components/TextButton';
+import { getUser } from "../../src/graphql/queries";
+import { Alert, View, Text, FlatList, StyleSheet } from "react-native";
+import { SearchBar } from "react-native-elements";
+import TextButton from "../base_components/TextButton";
+import Header from "../components/Header";
+import ParticipantsListItem from "../components/ParticipantsListItem";
 
 class AddParticipantScreen extends Component {
   constructor(props) {
@@ -29,20 +31,23 @@ class AddParticipantScreen extends Component {
 
   fetchRequestData = async () => {
     try {
-      const userData = await API.graphql(graphqlOperation(listUsersShortened))
-      console.log('fetch', this.props.participants)
-      this.setState({allData: userData.data.listUsers.items, participants: this.props.participants})
+      const userData = await API.graphql(graphqlOperation(listUsersShortened));
+      console.log("fetch", this.props.participants);
+      this.setState({
+        allData: userData.data.listUsers.items,
+        participants: this.props.participants
+      });
     } catch (e) {
-        console.log(e);
+      console.log(e);
     }
-  }
+  };
 
   componentDidMount() {
     this.didFocusListener = this.props.navigation.addListener(
-      'didFocus',
-      () => { 
-        this.fetchRequestData()
-      },
+      "didFocus",
+      () => {
+        this.fetchRequestData();
+      }
     );
   }
 
@@ -50,90 +55,128 @@ class AddParticipantScreen extends Component {
     this.didFocusListener.remove();
   }
 
-  _filterSearch = (text) => {
+  _filterSearch = text => {
     const { search, filteredData, allData } = this.state;
     if (text) {
-      const newData = allData.filter(function (item) {
+      const newData = allData.filter(function(item) {
         const itemData = item.username
           ? item.username.toUpperCase()
-          : ''.toUpperCase();
+          : "".toUpperCase();
         const textData = text.toUpperCase();
         return itemData.indexOf(textData) > -1;
       });
-      this.setState({filteredData: newData});
+      this.setState({ filteredData: newData });
     } else {
-      this.setState({filteredData: []}); //No query
+      this.setState({ filteredData: [] }); //No query
     }
-    this.setState({search: text});
+    this.setState({ search: text });
   };
 
-
-  renderUser(item){
-    console.log(item);
-    <TextButton title={item.username} primary onPress={() => this.addParticipant(item) }/>
+  renderUser(item) {
+    // console.log(item);
+    <TextButton
+      title={item.username}
+      primary
+      onPress={() => this.addParticipant(item)}
+    />;
   }
 
-  addParticipant(participant){
-    let participants = this.state.participants
-    let pids = participants.map(p => p.id)
-    if (participant.id == this.props.loggedInUser.id){
-      Alert.alert("Cannot add yourself as a participant.")
+  addParticipant(participant) {
+    let participants = this.state.participants;
+    let pids = participants.map(p => p.id);
+    if (participant.id == this.props.loggedInUser.id) {
+      Alert.alert("Cannot add yourself as a participant.");
     } else if (pids.includes(participant.id)) {
-      Alert.alert("Participant already added.")
+      Alert.alert("Participant already added.");
     } else {
-      participants.push(participant)
-      this.setState({ participants: participants})
+      participants.push(participant);
+      this.setState({ participants: participants });
     }
   }
 
   render() {
-    const { participants, search, filteredData, allData } = this.state
-    if (participants){
-      let displayParticipants = []
-      for(let i = 0; i < participants.length; i++){
+    const { participants, search, filteredData, allData } = this.state;
+    if (participants && this.props.loggedInUser) {
+      let displayParticipants = [];
+      for (let i = 0; i < participants.length; i++) {
         displayParticipants.push(
-          <Text key={participants[i].id}>{participants[i].username}</Text>
-        )
+          <Text key={participants[i].id}>
+            {participants[i].username}
+          </Text>
+        );
       }
       // let displayParticipants = participants.map(p => {
       //   <Text>{p.username}</Text>
       //   // <TextButton title={p.username} primary onPress={() => Actions.userProfileScreen({ userId: p.id })}/>
       // });
       return (
-        <View>
-          <View style={styles.participantList}>
-            <Text>
-              {displayParticipants}
-            </Text>
-          </View>
-  
+        <View style={styles.container}>
+          <Header title="Participants" />
+          <FlatList
+            style={styles.participantList}
+            horizontal={true}
+            data={participants}
+            renderItem={({ item }) => <ParticipantsListItem item={item} />}
+          />
+
           <SearchBar
+            style={styles.bar}
             placeholder="Search..."
-            onChangeText={(text) => this._filterSearch(text)}
+            onChangeText={text => this._filterSearch(text)}
             onClear={() => this._filterSearch("")}
             value={search}
           />
-  
+
           <FlatList
             data={filteredData}
             // keyExtractor={(item, index) => index.toString()}
             ItemSeparatorComponent={userItemSeparator}
-            renderItem={({ item }) => <TextButton title={item.username} primary onPress={() => this.addParticipant(item)}/>}
+            renderItem={({ item }) =>
+              <View style={styles.listItem}>
+                <TextButton
+                  style={styles.searchText}
+                  title={item.username}
+                  primary
+                  onPress={() => this.addParticipant(item)}
+                />
+              </View>}
           />
         </View>
-  
       );
     } else {
-      return (null)
+      return (
+        <View style={styles.container}>
+          <Text style={styles.loading}>Loading...</Text>
+        </View>
+      );
     }
   }
 }
 
 const styles = StyleSheet.create({
   participantList: {
-    paddingTop: 100,
-    padding: 50  
+    // marginBottom: 100
+  },
+  bar: {},
+  container: {
+    paddingTop: 60
+  },
+  loading: {
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  listItem: {
+    width: "100%",
+    padding: 15,
+    // backgroundColor: "#f8f8f8",
+    borderColor: "#c7c7c7",
+    borderBottomWidth: 1
+  },
+  searchText: {
+    fontSize: 20,
+    padding: 15
   }
-})
+});
 
 export default AddParticipantScreen;
