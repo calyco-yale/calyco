95360db6fb29f2859482e93fc0c5ae7409303b54
Yunji Jong <yunji.jong@yale.edu>
2021-04-28 01:13:49 +0900

    only show events ending after the current time in upcoming events

--- app/components/UpcomingEvent.js
+++ app/components/UpcomingEvent.js
@@ -32,24 +32,50 @@ class UpcomingEvent extends Component {
     return newEvents;
   };
 
+  getCurrentandAfterEvents = sortedEvents => {
+    const new_date = new Date();
+    const newEvents = [];
+    sortedEvents.forEach(event => {
+      // console.log(new_date)
+      // console.log(event.end_datetime);
+      const new_time = convertLocalTime(event.end_datetime);
+      const temp_date = new Date(new_time.substring(0,4), new_time.substring(5,7) - 1, new_time.substring(8,10), new_time.substring(11,13), new_time.substring(14,16), 0, 0);
+      if (temp_date >= new_date) {
+        newEvents.push(event);
+      }
+    });
+
+    return newEvents;
+  };
+
   //output upcoming events component that shows name, time, and ability to delete if logged in user
   render() {
     const events = this.props.events;
     const invitedEvents = this.props.invitedEvents;
-    const newInvitedEvents = [];
+    // console.log("invited");
+    // console.log(invitedEvents);
+    const newInvitedEvents = {};
+    const invitedList = [];
     for (let i = 0; i < invitedEvents.length; i++) {
-      if (invitedEvents[i] != null) {
-        newInvitedEvents.push(invitedEvents[i]);
+      if (invitedEvents[i]) {
+        if (!newInvitedEvents[invitedEvents[i].start_datetime]) {
+          newInvitedEvents[invitedEvents[i].start_datetime] = [];
+        }
+        newInvitedEvents[invitedEvents[i].start_datetime].push(invitedEvents[i]);
+        invitedList.push(invitedEvents[i]);
       }
     }
+    const mergedEvents = events.concat(invitedList);
     if (events) {
-      const sortedEvents = this.sortEvents(events);
+      const sortedEvents = this.sortEvents(mergedEvents);
+      const afterEvents = this.getCurrentandAfterEvents(sortedEvents);
+      // console.log(afterEvents);
       if (this.props.loggedIn) {
         return (
           <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           >
-            {sortedEvents.map(event => {
+            {afterEvents.map(event => {
               const start_time = convertLocalTime(event.start_datetime);
               const end_time = convertLocalTime(event.end_datetime);
               return (
@@ -85,7 +111,7 @@ class UpcomingEvent extends Component {
           <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           >
-            {sortedEvents.map(event => {
+            {afterEvents.map(event => {
               const start_time = convertLocalTime(event.start_datetime);
               const end_time = convertLocalTime(event.end_datetime);
               return (
